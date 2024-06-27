bd47252cc4062f59341f558f7a14e1641e403d8d
rafiq <rafiq.danova@gmail.com>
2024-06-27 22:48:25 +0800

    feat: query all menu

--- backend/controllers/MenuController.js
+++ backend/controllers/MenuController.js
@@ -1,7 +1,20 @@
+const prisma = require('../prisma/client')
+
 const findMenu = async (req, res) => {
+
+    const menus = await prisma.menu.findMany({
+        select: {
+            id: true,
+            name: true,
+            price: true,
+            description: true
+        }
+    })
+
     res.status(200).send({
         success: true,
-        message: "Get all menu successfully."
+        message: "Get all menu successfully.",
+        data: menus
     })
 }
 
